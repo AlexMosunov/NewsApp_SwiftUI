3c0d65e8c2fcbcf437ff7d51078fd9dd9cf59544
Oleksii Mosunov <alexmosunov@gmail.com>
2022-03-15 19:47:03 +0200

    Initial Commit

---
+++ NewsApp-SwiftUI/NewsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_SwiftUIApp.swift
+//  NewsApp-SwiftUI
+//
+//  Created by Alex Mosunov on 15.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
